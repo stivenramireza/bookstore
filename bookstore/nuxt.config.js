da40db5835d63158380e57d3d3f975cde866c64e
Stiven Ramírez Arango <sramirez@superfuds.com.co>
2021-09-04 17:41:35 -0500

    Web app with Docker

--- bookstore/nuxt.config.js
+++ bookstore/nuxt.config.js
@@ -1,7 +1,22 @@
+const path = require('path')
+const fs = require('fs')
+const env = process.env.NODE_ENV
+
+const envPath = path.resolve(
+  process.cwd(),
+  env !== 'development' ? `.env.${env}` : '.env.dev'
+)
+const defaultEnvPath = path.resolve(process.cwd(), '.env')
+
+require('dotenv').config({
+  path: fs.existsSync(envPath) ? envPath : defaultEnvPath
+})
+
+console.info(`Using ${env} environment variables`)
+
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'bookstore',
+    title: 'Bookstore',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -13,44 +28,30 @@ export default {
     ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module'
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa'
   ],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
       lang: 'en'
     }
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
 }
